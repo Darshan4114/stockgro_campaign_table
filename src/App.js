1d119ee05d7a4e9a25cecf653fd7a4c823be077e
Darshan Dhabale <darshan@darshandev.tech>
2022-05-09 22:12:17 +0530

    Added mui

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import TableRow from "@mui/material/TableRow";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
+import TextField from "@mui/material/TextField";
+import Pagination from "@mui/material/Pagination";
 import TableContainer from "@mui/material/TableContainer";
 
 import jsonData from "./data";
@@ -30,27 +32,68 @@ function App() {
   const handleSearchInput = (e) => {
     const val = e.target.value;
     if (val) {
-      const filteredCampaignList = jsonData.filter((campaign) => {
-        return campaign.name.toLowerCase().includes(val);
-      });
+      const filteredCampaignList = jsonData
+        .filter((campaign) => {
+          return campaign.name.toLowerCase().includes(val);
+        })
+        .slice(0, 10);
       setCampaignList(filteredCampaignList);
     } else {
-      setCampaignList(jsonData);
+      setCampaignList(
+        jsonData.slice((page.pgno - 1) * 10, (page.pgno - 1) * 10 + 10)
+      );
     }
     console.log("searching for", val);
   };
 
-  const handlePageChange = (pgno) => {
-    const curr = jsonData.slice((pgno - 1) * 10, (pgno - 1) * 10 + 10);
-    console.log("curr = ", pgno, curr, (pgno - 1) * 10, (pgno - 1) * 10 + 10);
-    setCampaignList(curr);
+  const handlePageChange = (e, pgno) => {
+    setPage({ pgno });
+    setCampaignList(jsonData.slice((pgno - 1) * 10, (pgno - 1) * 10 + 10));
   };
 
   return (
-    <div className="App">
+    <div className={styl.app}>
       <main>
-        <input className={styl.searchBar} onChange={handleSearchInput} />
-        <table>
+        <TextField
+          id="searchBar"
+          label="Search by name"
+          variant="outlined"
+          onChange={handleSearchInput}
+        />
+        <TableContainer className={styl.table} component={Paper}>
+          <Table
+            sx={{ minWidth: 650 }}
+            size="small"
+            aria-label="Campaign table"
+          >
+            <TableHead>
+              <TableRow>
+                {headers.length > 0 &&
+                  headers.map((header) => (
+                    <TableCell>
+                      {header.replace(/^\w/, (c) => c.toUpperCase())}
+                    </TableCell>
+                  ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {campaignList.length > 0 &&
+                campaignList.map((campaign) => (
+                  <TableRow
+                    key={campaign._id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    {headers.length > 0 &&
+                      headers.map((header) => (
+                        <TableCell>{campaign[header]}</TableCell>
+                      ))}
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+        {/* <table>
           <thead>
             {headers.length > 0 && headers.map((header) => <th>{header}</th>)}
           </thead>
@@ -63,15 +106,21 @@ function App() {
                 </tr>
               ))}
           </tbody>
-        </table>
-        <div className={styl.pagination}>
+        </table> */}
+        {/* <div className={styl.pagination}>
           {pageList.length > 0 &&
             pageList.map((page) => (
               <button onClick={() => handlePageChange(page.pgno)}>
                 {page.pgno}
               </button>
             ))}
-        </div>
+        </div> */}
+        <Pagination
+          className={styl.pagination}
+          count={10}
+          color="secondary"
+          onChange={handlePageChange}
+        />
       </main>
     </div>
   );
